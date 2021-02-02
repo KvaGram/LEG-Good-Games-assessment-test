a6eca7d8765079faff8eee853552e311d869a2db
KvaGram <0kvakk@gmailcom>
2021-02-02 11:28:12 +0100

    task 3 aborted - for now
    details in the wiki.
    Also gitignore updated to remove the bloated venv directory.

--- task 3/app/routes.py
+++ task 3/app/routes.py
@@ -1,6 +1,28 @@
 from app import app
+from flask import url_for
 
+fallback_image = url_for("static", filename="fallback.gif")
+fallback_name = "Mr. Testman"
+steamlogin_image = url_for("static", filename="steamlogin.png")
+
+#todo: move hardcoded html into templates/mysteam.html
 @app.route('/')
 @app.route('/mysteam')
 def mysteam():
-    return "Nothing here yet"
+    user= {"steamname" : fallback_name, "steampic" : fallback_image}
+    return """
+<html>
+    <head>
+        <title>Lars Erik Grambo - Good Games test task 3</title>
+    </head>
+    <body>
+        
+        <h1>Hello, """ + user["steamname"] + """!</h1>
+        <p>"""+ user["steampic"] +"""</p>
+        <img src=" """+ user["steampic"] +""" " alt="steam profile picture">
+
+        <p>hello world</p>
+    </body>
+</html>
+"""
+
---
+++ task 3/app/steamid.py
@@ -0,0 +1,4 @@
+from oic.oic import Client
+from oic.utils.Client import CLIENT_AUTHN_METHOD
+
+client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
