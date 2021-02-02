eb9a46433eca7ca16484a3a3079e2365b66329a0
KvaGram <0kvakk@gmailcom>
2021-02-02 14:29:02 +0100

    Add imported buttons and checks for steam session

--- task3b/index.php
+++ task3b/index.php
@@ -1,10 +1,34 @@
+<?php
+    require "steamauth/steamauth.php";
+    require "steamauth/userInfo.php";
 
+
+    if(isset($_SESSION["steamid"]))
+    {
+        $id = $_SESSION["steamid"];
+    }
+?>
 
 <html>
- <head>
-  <title>mysteam</title>
- </head>
- <body>
- <?php echo "<p>Hello World</p>"; ?> 
- </body>
+    <head>
+        <title>mysteam</title>
+    </head>
+    <body>
+        <?php echo "<p>Welcome to MySteam. A frivolous website that only does one singe thing."+
+        "\nAnd that is to allow you to log into steam, and then display your profile name and avatar."+
+        "\nThat's it. Also it took like the whole day to do, becouse apperently I am a moron when to comes to webdevelopment.</p>";
+        if(isset($id))
+        {
+        #insert the logout button
+        echo logoutbutton();
+        }
+        else
+        {
+
+        #insert the logout button
+        echo loginbutton("rectangle");
+        }
+
+        ?> 
+    </body>
 </html>
