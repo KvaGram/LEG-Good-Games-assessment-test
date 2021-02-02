126e05221ebb889ac5a3677b6d24be88f02dbb17
KvaGram <0kvakk@gmailcom>
2021-02-02 14:40:43 +0100

    fix mistakes
    The userinfo import seem to require session to be set. Makes sense. moved it inside the if-isset block.
    The initial welcome message is static, so it have been moved outside of a php statement.

--- task3b/index.php
+++ task3b/index.php
@@ -1,11 +1,9 @@
 <?php
     require "steamauth/steamauth.php";
-    require "steamauth/userInfo.php";
-
-
     if(isset($_SESSION["steamid"]))
     {
         $id = $_SESSION["steamid"];
+        require "steamauth/userInfo.php";
     }
 ?>
 
@@ -14,21 +12,25 @@
         <title>mysteam</title>
     </head>
     <body>
-        <?php echo "<p>Welcome to MySteam. A frivolous website that only does one singe thing."+
-        "\nAnd that is to allow you to log into steam, and then display your profile name and avatar."+
-        "\nThat's it. Also it took like the whole day to do, becouse apperently I am a moron when to comes to webdevelopment.</p>";
-        if(isset($id))
-        {
-        #insert the logout button
-        echo logoutbutton();
-        }
-        else
-        {
-
-        #insert the logout button
-        echo loginbutton("rectangle");
-        }
-
+        <p>
+            Welcome to MySteam. A frivolous website that only does one singe thing.
+            And that is to allow you to log into steam, and then display your profile name and avatar.
+            That's it. Also it took like the whole day to do, becouse apperently I am a moron when to comes to webdevelopment.
+        </p>
+        
+        <?php
+            if(isset($id))
+            {
+                #todo: display name and avatar
+                #insert the logout button
+                echo logoutbutton();
+            }
+            else
+            {
+                echo "<p> You are currently not logged in. Press the button below to log in.</p>";
+                #insert the logout button
+                echo loginbutton("rectangle");
+            }
         ?> 
     </body>
 </html>
