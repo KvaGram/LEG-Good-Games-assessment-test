5ffc6293aa327fa0f959a3b55762f41e87a21168
KvaGram <0kvakk@gmailcom>
2021-02-03 12:50:16 +0100

    Added filter for the character a

--- task2/gamelist.php
+++ task2/gamelist.php
@@ -7,7 +7,30 @@
         <?php
             $steamappsraw = file_get_contents("https://api.steampowered.com/ISteamApps/GetAppList/v2/");
             $steamapps = json_decode($steamappsraw);
-            echo $steamapps -> applist -> apps[0];
+            $list = $steamapps -> applist -> apps;
+            $list_length = sizeof($list);
+            echo "number of apps : " . $list_length . "<br>";
+
+            $sample = $list[123];
+            #var_dump($sample);
+            #echo $sample -> name;
+
+            $gameidstoload = [];
+            $filter_1_reject_count = 0;
+            foreach($list as $g)
+            {
+                $name = $g->name;
+                $id = $g->appid;
+                if(strpos($name, "a"))
+                {
+                    $filter_1_reject_count ++;
+                }
+                else
+                {
+                    array_push($gameidstoload, $id);
+                }
+            }
+            echo "filter 1. Do not contain character 'a'. rejected ".$filter_1_reject_count." entries";
         ?>
 
     </head>
